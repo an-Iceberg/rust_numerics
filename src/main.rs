a60a52a02848770681ccd074586f7e9e3fad2e6c
Sandra <a.g.marosi@bluewin.ch>
2024-04-21 22:35:35 +0200

    Refactorings; added partial deriv && integral
    Added partial derivative using midpoint method
    Added integration over interval using simpson's rule

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
+use crate::num::linalg::Matrix;
+
 mod num;
 #[cfg(test)]
 mod numerics_tests;
 
 fn main()
 {
-  fn f(x: f64) -> f64 { return x.powi(2) + x - 1.; }
+  fn f(x: f64) -> f64 { x.powi(2) + x - 1. }
+  fn f1(x: Vec<f64>) -> f64 { x[0].powi(2) - x[1].powi(2) }
 
-  println!("f(2) = {}", f(2.));
-  println!("f'(2) = {:.3}", num::deriv1_2(f, 2.));
+  println!("f1([3,2]) = {}", f1(vec![3., 2.]));
+
+  println!("f(3) should:11 is:{}", f(3.));
+  println!("f(4) should:19 is:{}", f(4.));
+  println!("f'(3) should:7 is:{} ε:{}", num::deriv(f, 3., 1), (num::deriv(f, 3., 1) - 7.).abs());
+  println!("f'(4) should:9 is:{} ε:{}", num::deriv(f, 4., 1), (num::deriv(f, 4., 1) - 9.).abs());
   println!();
-  println!("f(2) = {}", f(2.));
-  println!("f'(2) = {:.3}", num::deriv1_5(f, 2.));
+  println!("{:?}", num::bisection(f, -10., 0., 1e-20));
   println!();
-  println!("f(-1) = {}", f(-1.));
-  println!("f'(-1) = {:.3}", num::deriv1_2(f, -1.));
+  println!("{:?}", num::newton_zero(f, 100., 1e-10));
   println!();
-  println!("{:.3?}", num::bisection(f, -10., 0., 1e-20));
-  println!();
-  println!("{:.3?}", num::newton_zero(f, 100., 1e-5));
-  println!();
-  println!("{:.3?}", num::secant_zero(f, 100., 98., 1e-5));
+  println!("{:?}", num::secant_zero(f, 100., 98., 1e-10));
+
+  #[allow(non_snake_case)]
+  let A: Matrix = vec![
+    vec![1., 2., 3.],
+    vec![4., 5., 6.],
+    vec![7., 8., 9.],
+  ];
+  println!("\nA[2][0] ⇒  should:7 is:{}\n", A[2][0]);
+  // ↑ this is good, it means we can take the indices 1 to 1 from the math formulas
+  num::linalg::print_mat(&A);
+  // println!("{}", 1e-5);
+  // println!("{}", 10e-5);
 }
--- src/num.rs
+++ src/num.rs
@@ -1,32 +1,102 @@
 // TODO: docs
+// TODO: tests
+// TODO: generics, so that any numeric type can be used
 
 use std::ops::{Div, Mul};
 
-/// Calculates the first derivative (f'(x)) of `f` at `x` using the formula for
-/// the definition of the derivative.
-pub fn deriv1(f: fn(f64) -> f64, x: f64) -> f64
+// ToDo: create one fn `deriv` which then calls different functions based on parameters
+// such as dergee and which method (standard, 2 point, 5 point)
+
+pub fn deriv(f: fn(f64) -> f64, x: f64, degree: u8) -> f64
 {
-  let h = 0.001;
-  return (f(x + h) - f(x)) / h;
+  // ToDo: determine h based on f64::EPSILON
+  // I think it is more efficient to have a sort of look-up table for the derivatives
+  // instead of using newton's formula https://en.wikipedia.org/wiki/Numerical_differentiation#Higher_derivatives
+  let h = 10e-4;
+  match degree
+  {
+    0 => f(x),
+    1 => (f(x+h) - f(x-h)) / (2.*h),
+    2 => (f(x+h) - 2.*f(x) + f(x-h)) / (h.powi(2)),
+    _ => todo!(),
+  }
+  // Note: 5 point method: (f(x - 2.*h) - 8.*f(x - h) + 8.*f(x + h) - f(x + 2.*h)) / (12. * h)
 }
 
-/// Calculates the first derivative (f'(x)) of `f` at `x` using a more precise
-/// formula.
-pub fn deriv1_2(f: fn(f64) -> f64, x: f64) -> f64
+pub fn partial(f: fn(&Vec<f64>) -> f64, x: &Vec<f64>, i: usize, degree: u8) -> f64
 {
-  let h = 0.001;
-  return (f(x + h) - f(x - h)) / (2. * h);
+  if i >= x.len()
+  {
+    panic!("{}", format!("Index i = {} out of range for x of length {}", i, x.len()));
+  }
+
+  let h = 10e-4;
+  match degree
+  {
+    0 => f(x),
+    1 =>
+    {
+      let mut x_plus_h = x.clone();
+      let mut x_minus_h = x.clone();
+      x_plus_h[i] += h;
+      x_minus_h[i] -= h;
+      (f(&x_plus_h) - f(&x_minus_h)) / (2. * h)
+    },
+    2 =>
+    {
+      let mut x_plus_h = x.clone();
+      let mut x_minus_h = x.clone();
+      x_plus_h[i] += h;
+      x_minus_h[i] -= h;
+      (f(&x_plus_h) - (2.*f(x)) + f(&x_minus_h)) / h.powi(2)
+    }
+    _ => todo!()
+  }
 }
 
-/// Calculates the first derivative (f'(x)) of `f` at `x` using a 5-point stencil
-/// method.
-pub fn deriv1_5(f: fn(f64) -> f64, x: f64) -> f64
+fn nCr(n: u64, r: u64) -> f64
 {
-  // Todo: implement ⇒ https://en.wikipedia.org/wiki/Numerical_differentiation#Higher-order_methods
-  let h = 0.001;
-  return (f(x - 2.*h) - 8.*f(x - h) + 8.*f(x + h) - f(x + 2.*h)) / (12. * h);
+  // Note: https://www.geeksforgeeks.org/program-calculate-value-ncr/
+  todo!()
 }
 
+fn fact(n: u64) -> u64
+{
+  // Note: https://users.rust-lang.org/t/whats-the-idiomatic-or-preferred-way-of-calculating-factorials/77684/3
+  todo!()
+}
+
+/// Calculates the area under the curve (integral) of the function `f` using
+/// simpson's rule. `h` is the precision.
+pub fn int(f: fn(f64) -> f64, a: f64, b: f64, h: f64) -> f64
+{
+  let n = ((b - a) / h).ceil() as u64;
+  #[allow(non_snake_case)]
+  let mut Σ1 = 0.;
+  #[allow(non_snake_case)]
+  let mut Σ2 = 0.;
+
+  let x = |i| a + (i as f64 * h);
+
+  for i in 1..n
+  { Σ1 += f(x(i)); }
+
+  for (i, j) in (0..n).zip((0..n).skip(1))
+  { Σ2 += f((x(i) + x(j)) / 2.); }
+
+  (h/3.)*((f(a)/2.) + Σ1 + Σ2.mul(2.) + (f(b)/2.))
+}
+
+pub fn int_rect(f: fn(f64) -> f64, a: f64, b: f64, h: f64) -> f64
+{
+  todo!()
+}
+
+// Note: https://en.wikipedia.org/wiki/Root-finding_algorithms
+// Note: https://en.wikipedia.org/wiki/Householder%27s_method
+// Note: https://en.wikipedia.org/wiki/Gauss%E2%80%93Seidel_method
+
+// Todo: tolerance should take ε into account
 /// [Formula](https://en.wikipedia.org/wiki/Nested_intervals)
 pub fn bisection(f: fn(f64) -> f64, a: f64, b: f64, tolerance: f64) -> Result<f64, String>
 {
@@ -64,6 +134,7 @@ pub fn bisection(f: fn(f64) -> f64, a: f64, b: f64, tolerance: f64) -> Result<f6
 /// [Formula](https://en.wikipedia.org/wiki/Secant_method)
 pub fn secant_zero(f: fn(f64) -> f64, x0: f64, x1: f64, tolerance: f64) -> Option<f64>
 {
+  // ToDo: fail states
   let mut x1 = x0;
   let mut x2 = x0 + tolerance.mul(2.);
   let mut x3 = x0 + tolerance.mul(4.);
@@ -81,13 +152,14 @@ pub fn secant_zero(f: fn(f64) -> f64, x0: f64, x1: f64, tolerance: f64) -> Optio
 /// [Formula](https://en.wikipedia.org/wiki/Newton%27s_method#)
 pub fn newton_zero(f: fn(f64) -> f64, x0: f64, tolerance: f64) -> Option<f64>
 {
+  // ToDo: fail states
   let mut x1 = x0;
   let mut x2 = x1 + 2. * tolerance;
 
   while (x1 - x2).abs() > tolerance
   {
     x1 = x2;
-    x2 = x1 - f(x1).div(deriv1(f, x1));
+    x2 = x1 - f(x1).div(deriv(f, x1, 1));
   }
 
   return Some(x2);
@@ -95,37 +167,60 @@ pub fn newton_zero(f: fn(f64) -> f64, x0: f64, tolerance: f64) -> Option<f64>
 
 pub mod linalg
 {
-  type Matrix = Vec<Vec<f64>>;
-  type Vector = Vec<f64>;
+  pub type Matrix = Vec<Vec<f64>>;
+  pub type Vector = Vec<f64>;
 
-  pub fn zero(n: usize) -> Matrix
+  #[allow(non_snake_case)]
+  pub fn mat_to_string(M: &Matrix) -> String
   {
     todo!()
   }
 
+  pub fn vec_to_string(a: Vector) -> String
+  {
+    todo!()
+  }
+
+  pub fn zero(n: usize) -> Matrix
+  {
+    return vec![vec![0.; n]; n];
+  }
+
   pub fn id(n: usize) -> Matrix
+  {
+    #[allow(non_snake_case)]
+    let mut M = zero(n);
+    for i in 0..n { M[i][i] = 1.; }
+    return M;
+  }
+
+  #[allow(non_snake_case)]
+  pub fn matmul(A: &Matrix, B: &Matrix) -> Result<Matrix, String>
   {
     todo!()
   }
 
   #[allow(non_snake_case)]
-  pub fn matmul(A: Matrix, B: Matrix) -> Result<Matrix, String>
+  pub fn lr(M: &Matrix) -> Result<(Matrix, Matrix), String>
   {
     todo!()
   }
 
-  pub fn lr(M: Matrix) -> (Matrix, Matrix)
+  #[allow(non_snake_case)]
+  pub fn qr(M: &Matrix) -> Result<(Matrix, Matrix), String>
   {
     todo!()
   }
 
-  pub fn qr(M: Matrix) -> (Matrix, Matrix)
+  #[allow(non_snake_case)]
+  pub fn gauss_seidel(A: &Matrix, b: &Vector) -> Result<Vector, String>
   {
     todo!()
   }
 
-  pub fn gauss_seidel(A: Matrix, b: Vector) -> Vector
+  pub fn print_mat(A: &Matrix)
   {
-    todo!()
+    A.iter()
+      .for_each(|vec| println!("{:?}", vec));
   }
 }
